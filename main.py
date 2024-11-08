487ed92f970cfae57910a55adfd88f03b91e5600
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-11-08 17:25:26 -0300

    002: base files for game

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import pygame
+
+from constants import *
+
+
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    exit = 0
+    while not exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((20, 20, 40))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
