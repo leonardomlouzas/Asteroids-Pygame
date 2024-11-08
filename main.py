70fa1e19b1f90988ddca8ff1092587dcf32402c4
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-11-08 17:54:34 -0300

    003: player draw

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     exit = 0
     while not exit:
@@ -15,7 +21,10 @@ def main():
                 return
 
         screen.fill((20, 20, 40))
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
