653cbf3c06667f147f8de44c1793dec3a69cb26b
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-11-08 19:47:36 -0300

    007: asteroid and field implementation in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -8,17 +10,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
-
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    updatable.add(player)
-    drawable.add(player)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
+
+    field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     exit = 0
     while not exit:
@@ -26,7 +30,6 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000
         screen.fill((20, 20, 40))
 
         for item in updatable:
@@ -36,6 +39,7 @@ def main():
             item.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
